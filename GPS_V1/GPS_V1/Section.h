c3ad81275c2f27f6373577860985f36df53a01ff
Lefix2 <ftrefou@gmail.com>
2015-03-03 01:11:08 +0100

    Premiers headers

---
+++ GPS_V1/GPS_V1/Section.h
@@ -0,0 +1,13 @@
+#ifndef SECTION_H
+#define SECTION_H
+
+#include "common.h"
+
+typedef struc section{
+	int id;
+	int type;
+	int pos[2];
+	int size[2];
+}section;
+
+#endif
---
+++ GPS_V1/GPS_V1/common.h
@@ -0,0 +1,13 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define X 0
+#define Y 1
+
+typedef enum bool{ false , true};
+typedef enum type{ wall, floor, section, promo, checkout, entrance, reception};
+
+#endif // !COMMON_H
