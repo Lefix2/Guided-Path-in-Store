3035eedb91d750f34ab27f3c5aab8e8c7da43fed
rom29 <roman.pavec@hotmail.fr>
2015-05-06 21:57:45 +0200

    base de données

---
+++ GPS_V1/GPS_V1/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sqlite3.h"
+#include <string.h> //Fonctions strcpy, strcat
+
+#define MAX_ARRAY_OF_CHAR 255
+
+int main(int argc, char* argv[])
+{
+	//createTable();
+	//printf("%d\n",returnRow("C:/Users/rom/Documents/projet.db"));
+	//system("PAUSE");
+	char **tab;
+	tab = (char**)malloc(MAX_ARRAY_OF_CHAR*sizeof(char*));
+	//tab = returnValue();
+	tab = element_list("C:/Users/rom/Documents/projet.db");
+	int i;
+	for (i = 0; i < 8; i++)
+	{
+		printf("%s \n", tab[i]);
+		}
+	system("pause");
+}
