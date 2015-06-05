8a5710880eb465733c00f36fbc0f0366641cc7b5
Lefix2 <ftrefou@gmail.com>
2015-06-05 18:00:16 +0200

    commentaires generalmenu

--- GPS_final/GPS_final/generalMenu.h
+++ GPS_final/GPS_final/generalMenu.h
@@ -1,17 +1,29 @@
+/*!
+* \file  generalMenu.h
+* \brief Header of generalMenu.c
+* \author GPS team
+* \date 13/12/2014
+*
+* Contain prototype of itemList
+*/
 #ifndef GENERALMENU_H
 #define GENERALMENU_H
 
 #include "Common.h"
 
+/**
+* \struct generalmenu_struct
+* \brief Structure for the menu
+*/
 typedef struct generalmenu_struct generalmenu_struct;
 struct generalmenu_struct{
 
-	shopping *myShop;
+	shopping *myShop;			/*!< pointer to a shop */
 
-	GtkWidget * main_window;
-	GtkWidget * button_selec;
-	GtkWidget * button_makel;
-	GtkWidget * button_gosho;
+	GtkWidget * main_window;	/*!< the window */
+	GtkWidget * button_selec;	/*!< the selection button */
+	GtkWidget * button_makel;	/*!< the making list button */
+	GtkWidget * button_gosho;	/*!< the shopping button */
 };
 
 void call_main_menu(GtkApplication *app);
